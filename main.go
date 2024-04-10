456e53ba74e5be523ea5a32a48ac7221e64eb471
best2000 <silentman@hotmail.co.th>
2024-04-11 00:46:20 +0700

    dev

--- main.go
+++ main.go
@@ -10,19 +10,22 @@ import (
 
 func main() {
 
-	root := "/mnt/d/torrent"
+	root := os.Args[1]
+	pattern := os.Args[2]
 
-	// var files []string
+	fmt.Println("root: \"",root+"\"")
+	fmt.Println("pattern: \"",pattern+"\"")
+
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
 		if !info.IsDir() {
 			//is file
 			name := info.Name()
-			pattern := `(hhd800.com@)+`
 			//(?i)((snis)|(ssni)|(dvdms)|(sdmua)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)|(fsdss)){1}[-]{0,1}[\d]{1,4}
 			//[\w]{3,5}[-]{0,1}[\d]{1,4}
 			match, err := regexp.MatchString(pattern, name)
 			if err == nil && match {
-				
+
 				renamed := strings.Replace(path, "hhd800.com@", "", 1)
 				fmt.Println(path + " => " + renamed)
 				err := os.Rename(path, renamed)
